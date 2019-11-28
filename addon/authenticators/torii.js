d3e92e5ff09fd024a57b970ecb3f900a89ae8e3f
Erika Pauwels <erika.pauwels@gmail.com>
2019-11-28 16:03:15 +0100

    Use fetch instead of ajax for backend requests

--- addon/authenticators/torii.js
+++ addon/authenticators/torii.js
@@ -1,43 +1,63 @@
 import { inject as service } from '@ember/service';
 import ToriiAuthenticator from 'ember-simple-auth/authenticators/torii';
+import fetch, { Headers } from 'fetch';
 
 /**
  * ACM/IDM OAuth2 authenticator
 */
 export default ToriiAuthenticator.extend({
   torii: service(),
-  ajax: service(),
 
   basePath: 'sessions',
   toriiProvider: 'acmidm-oauth2',
 
-  authenticate() {
-    return this._super(...arguments).then((data) => { // get authorization code through Torii
-      return this.ajax.request(this.basePath, {
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'application/vnd.api+json',
-        data: JSON.stringify({
-          authorizationCode: data.authorizationCode
-        })
-      }).then((response) => {
-        response.provider = this.get('toriiProvider'); // required to make session restore work
-        return response;
-      });
+  async authenticate() {
+    const data = await this._super(...arguments); // get authorization code through Torii
+    const result = await fetch(this.basePath, {
+      type: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/vnd.api+json'
+      }),
+      body: JSON.stringify({
+        authorizationCode: data.authorizationCode
+      })
     });
+
+    if (result.ok) {
+      const response = result.json();
+      response.provider = this.get('toriiProvider'); // required to make session restore work
+      return response;
+    } else {
+      throw result;
+    }
   },
 
-  invalidate() {
-    return this._super(...arguments).then( () => {
-      const path = `/${this.basePath}/current`;
-      return this.ajax.del(path);
+  async invalidate() {
+    const result = await fetch(`${this.basePath}/current`, {
+      method: 'DELETE',
+      headers: new Headers({
+        'Content-Type': 'application/vnd.api+json'
+      })
     });
+
+    if (result.ok)
+      return result;
+    else
+      throw result;
   },
 
-  restore() {
-    return this._super(...arguments).then( () => {
-      const path = `/${this.basePath}/current`;
-      return this.ajax.request(path);
+  async restore() {
+    await this._super(...arguments);
+    const result = await fetch(`${this.basePath}/current`, {
+      type: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/vnd.api+json'
+      })
     });
+
+    if (result.ok)
+      return result.json();
+    else
+      throw result;
   }
 });
