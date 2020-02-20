45a094d12d5ff2afca37aa2f65968b404599ea95
Claire Lovisa <lovisa.claire@gmail.com>
2020-02-20 13:42:43 +0100

    Use fetch instead of ajax for switching accounts

--- addon/components/acmidm-switch.js
+++ addon/components/acmidm-switch.js
@@ -1,11 +1,10 @@
 import Component from '@ember/component';
 import layout from '../templates/components/acmidm-switch';
 import { configurable } from 'torii/configuration';
-import { inject } from '@ember/service';
+import fetch from 'fetch';
 
 export default Component.extend({
   layout,
-  ajax: inject(),
   name: Object.freeze('acmidm-oauth2'), // used by configurable
   logoutUrl: configurable('logoutUrl'),
   clientId: configurable('apiKey'),
@@ -17,7 +16,9 @@ export default Component.extend({
   },
   async click() {
       this.set('disabled', true);
-      await this.ajax.del('/sessions/current');
+      await fetch('/sessions/current', {
+        method: 'DELETE'
+      });
       window.location.replace(`${this.logoutUrl}?switch=true&client_id=${this.clientId}&post_logout_redirect_uri=${encodeURIComponent(this.returnUrl)}`);
   }
 });
