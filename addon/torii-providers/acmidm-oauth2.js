9ad4085ebe437a6a2a3b0900919a5772e9665891
Erika Pauwels <erika.pauwels@gmail.com>
2021-08-28 13:01:14 +0200

    Refactor acmidm-oauth2 torii-provider to native class

--- addon/torii-providers/acmidm-oauth2.js
+++ addon/torii-providers/acmidm-oauth2.js
@@ -1,36 +1,37 @@
 import Oauth2 from 'torii/providers/oauth2-code';
-import { configurable } from 'torii/configuration';
+import { getOwner } from '@ember/application';
 
 /**
  * This class implements authentication against ACM/IDM
  * using the OAuth2 authorization flow in a popup window.
  */
-export default Oauth2.extend({
-  name: 'acmidm-oauth2',
+export default class AcmidmOAuth2 extends Oauth2 {
+  name = 'acmidm-oauth2';
+  requiredUrlParams = ['response_type', 'client_id', 'redirect_uri', 'state'];
+  optionalUrlParams = 'scope';
+  responseParams = 'code';
 
-  baseUrl: configurable('baseUrl'),
+  constructor() {
+    super(...arguments);
+    const config =
+      getOwner(this).resolveRegistration('config:environment')?.torii;
+    const providerConfig = config?.providers
+      ? config.providers[this.name]
+      : null;
+    if (!providerConfig) {
+      throw `Could not find ${this.name} configuration, make sure it is set in your application environment`;
+    }
 
-  requiredUrlParams: Object.freeze([
-    'response_type',
-    'client_id',
-    'redirect_uri',
-    'state',
-  ]),
-  optionalUrlParams: Object.freeze(['scope']),
-  responseParams: Object.freeze(['code']),
-
-  scope: configurable('scope', 'openid'),
-
-  redirectUri: configurable('redirectUri', function () {
-    return this._super();
-  }),
+    this.baseUrl = providerConfig.baseUrl;
+    this.scope = providerConfig.scope || 'openid';
+    this.redirectUri = providerConfig.redirectUri;
+  }
 
   open(options) {
-    return this._super(options);
-  },
-});
     const opts = Object.assign({}, options, {
       resizable: true,
       scrollbars: true,
     });
-    return this._super(opts);
+    return super.open(opts);
+  }
+}
