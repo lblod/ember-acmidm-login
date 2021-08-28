ffef022837f8ac885461f1050079c7d09718a577
Erika Pauwels <erika.pauwels@gmail.com>
2021-08-28 12:03:46 +0200

    Refactor session authentication promise with async/await

--- addon/components/acmidm-login.js
+++ addon/components/acmidm-login.js
@@ -10,22 +10,22 @@ export default class AcmIdmLoginComponent extends Component {
   @service session;
 
   @action
-  login() {
+  async login() {
     this.errorMessage = '';
     this.isAuthenticating = true;
-    this.session
-      .authenticate('authenticator:torii', 'acmidm-oauth2')
-      .catch((reason) => {
-        warn(reason.error || reason, { id: 'authentication.failure' });
 
-        if (reason.status == 403)
-          this.errorMessage = 'U heeft geen toegang tot deze applicatie.';
-        else
-          this.errorMessage =
-            'Fout bij het aanmelden. Gelieve opnieuw te proberen.';
-      })
-      .finally(() => {
-        this.isAuthenticating = false;
-      });
+    try {
+      await this.session.authenticate('authenticator:torii', 'acmidm-oauth2');
+    } catch (reason) {
+      warn(reason.error || reason, { id: 'authentication.failure' });
+
+      if (reason.status == 403)
+        this.errorMessage = 'U heeft geen toegang tot deze applicatie.';
+      else
+        this.errorMessage =
+          'Fout bij het aanmelden. Gelieve opnieuw te proberen.';
+    } finally {
+      this.isAuthenticating = false;
+    }
   }
 }
