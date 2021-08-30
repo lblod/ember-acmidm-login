4614c94a9d1a67b337610509a2a29ff713f69b93
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-08-30 16:45:41 +0200

    Deprecate the `<AcmidmSwitch>` component
    This component can easily be replaced by the `<Acmidm::Switch>` provider component. This has the benefit that the consuming project has complete control over the styling.

--- addon/components/acmidm-switch.js
+++ addon/components/acmidm-switch.js
@@ -1,4 +1,21 @@
-import templateOnly from '@ember/component/template-only';
+import Component from '@glimmer/component';
+import { deprecate } from '@ember/debug';
 
-const AcmidmSwitchComponent = templateOnly();
-export default AcmidmSwitchComponent;
+export default class AcmidmSwitchComponent extends Component {
+  constructor() {
+    super(...arguments);
+
+    deprecate(
+      'The <AcmidmSwitch> component is deprecated. Please use the <Acmidm::Switch> provider component instead.',
+      false,
+      {
+        id: '@lblod/ember-acmidm-login.acmidm-switch.component',
+        for: '@lblod/ember-acmidm-login',
+        since: {
+          enabled: '1.3.0',
+        },
+        until: '2.0.0',
+      }
+    );
+  }
+}
