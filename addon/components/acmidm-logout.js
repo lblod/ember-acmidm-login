f7f7ae807af8274757b731929c1a110da43131c9
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-08-30 16:55:48 +0200

    Deprecate the `<AcmidmLogout>` component
    This component is a simple wrapper around the `invalidate` method of the session service. Since invalidating the session doesn't require any specific logic for Acm/Idm, it doesn't really need to be part of this addon either.

--- addon/components/acmidm-logout.js
+++ addon/components/acmidm-logout.js
@@ -1,9 +1,27 @@
 import Component from '@glimmer/component';
+import { deprecate } from '@ember/debug';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class AcmidmLogoutComponent extends Component {
   @service session;
+
+  constructor() {
+    super(...arguments);
+
+    deprecate(
+      'The <AcmidmLogout> component is deprecated. Please use the `invalidate` method of the `session` service instead.',
+      false,
+      {
+        id: '@lblod/ember-acmidm-login.acmidm-logout.component',
+        for: '@lblod/ember-acmidm-login',
+        since: {
+          enabled: '1.3.0',
+        },
+        until: '2.0.0',
+      }
+    );
+  }
 
   @action
   logout() {
