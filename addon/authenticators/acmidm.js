541bee8d83d8168eb5a128e840c91dac4c6e6d8f
Niels Vandekeybus <progster@gmail.com>
2021-10-07 14:30:02 +0200

    happy flow iframe login

---
+++ addon/authenticators/acmidm.js
@@ -0,0 +1,48 @@
+import Base from 'ember-simple-auth/authenticators/base';
+const basePath = '/sessions';
+
+export default class AcmidmAuthenticator extends Base {
+  async restore() {
+    const result = await fetch(`${basePath}/current`, {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/vnd.api+json',
+      }),
+    });
+
+    if (result.ok) {
+      const response = await result.json();
+    }
+  }
+
+  async authenticate(authorizationCode) {
+    const result = await fetch(basePath, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/vnd.api+json',
+      }),
+      body: JSON.stringify({
+        authorizationCode: authorizationCode,
+      }),
+    });
+
+    if (result.ok) {
+      const response = await result.json();
+      return response;
+    } else {
+      throw result;
+    }
+  }
+
+  async invalidate() {
+    const result = await fetch(`${basePath}/current`, {
+      method: 'DELETE',
+      headers: new Headers({
+        'Content-Type': 'application/vnd.api+json',
+      }),
+    });
+
+    if (result.ok) return result;
+    else throw result;
+  }
+}
---
+++ addon/components/acmidm-login-iframe.js
@@ -0,0 +1,57 @@
+import Component from '@glimmer/component';
+import { getOwner } from '@ember/application';
+import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+const basePath = '/sessions';
+
+export default class AcmidmLoginIframeComponent extends Component {
+  @service session;
+  @tracked errorMessage = '';
+  @tracked isAuthenticating = false;
+  providerConfig;
+  url;
+
+  constructor() {
+    super(...arguments);
+
+    const config =
+      getOwner(this).resolveRegistration('config:environment')?.torii;
+
+    this.providerConfig = config?.providers
+      ? config.providers['acmidm-oauth2']
+      : null;
+
+    if (!this.providerConfig) {
+      throw 'could not find acmidm-oauth2 configuration, make sure it is set in your application environment';
+    }
+    this.url =
+      `${this.providerConfig.baseUrl}?response_type=code&` +
+      `client_id=${this.providerConfig.apiKey}&` +
+      `redirect_uri=${encodeURIComponent(this.providerConfig.redirectUri)}&` +
+      `scope=${this.providerConfig.scope}`;
+  }
+
+  @action
+  async detectLogin(event) {
+    const iframe = event.target;
+    if (iframe) {
+      try {
+        // this will only succeed if we return to our own app;
+        const location = iframe.contentWindow.location;
+        const params = new URL(location).searchParams;
+        const authorizationCode = params.get('code');
+        if (!authorizationCode) {
+          this.errorMessage = 'Did not receive auth code';
+        }
+        this.session.authenticate('authenticator:acmidm', authorizationCode);
+      } catch (e) {
+        // assume we loaded the remote href
+        this.errorMessage = e;
+        console.error(e);
+      }
+    }
+  }
+}
